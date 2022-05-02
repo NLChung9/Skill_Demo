57d08c9683c53d3a4f64e13a4ff3a21e342fd26c
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-05-01 22:33:29 -0700

    Added File, File Tester, and .jar files for JUNIT

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,6 +1,11 @@
+//Github URL: https://github.com/NLChung9/Skill_Demo.git
+
 public class SkillDemo{
     //Continue in VSCode
     //SkillDemo.multiply();
     //public static int mltiply(){
-    
+
+    public static int multiply(int num1, int num2){
+        return num1*num2;
+    }
 }
